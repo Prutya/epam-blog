8fa698ca324291299760d3030a4f9a526a9b9b9d
Anton <antonprudkoglyad@gmail.com>
2016-01-19 17:20:09 +0200

    author index view redesign + start detailsViewModel + details view

--- EpamBlog.Web/Models/Author/DetailsViewModel.cs
+++ EpamBlog.Web/Models/Author/DetailsViewModel.cs
@@ -7,6 +7,16 @@ namespace EpamBlog.Web.Models.Author
 {
     public class DetailsViewModel
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
 
+        public IEnumerable<Article.IndexViewModel> Articles { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1} {2} ({3})", Id, FirstName, LastName, Email);
+        }
     }
 }
